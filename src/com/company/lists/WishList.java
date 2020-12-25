001c47054b9fb49726e08a853eae2c0f5449a847
Ivan Valentinov <ivan156drs@gmail.com>
2020-12-25 21:43:08 +0200

    Updated classes.

--- src/com/company/lists/WishList.java
+++ src/com/company/lists/WishList.java
@@ -1,4 +1,22 @@
 package com.company.lists;
 
-public class WishList {
+import java.util.ArrayList;
+
+public class WishList extends ProductContainer{
+
+    public WishList(){
+        products = new ArrayList<>();
+    }
+
+    @Override
+    public void printList() {
+        try{
+            if(products.isEmpty()){
+                //throw new Exception(String.format("——— The wishlist of %s does not contain anything! ———"));
+            }
+
+        } catch (Exception exception){
+            System.out.println(exception.getMessage());
+        }
+    }
 }
