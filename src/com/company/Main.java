b4a4f1e14c64c62ff41b8e6aff5910deaaae71ad
Ivan Valentinov <ivan156drs@gmail.com>
2020-12-25 19:50:12 +0200

    Updated Main file.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ import com.company.people.Publisher;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         Publisher p1 = new Publisher("Aux", "EU");
         p1.publishBook("1113");
         p1.publishBook("In the beginning");
