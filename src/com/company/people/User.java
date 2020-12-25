6d21917117a3e7ef54a12d4c5621bb426476ffa3
Ivan Valentinov <ivan156drs@gmail.com>
2020-12-25 21:42:12 +0200

    Updated classes.

--- src/com/company/people/User.java
+++ src/com/company/people/User.java
@@ -1,5 +1,13 @@
 package com.company.people;
 
+import com.company.lists.Cart;
+import com.company.lists.WishList;
+
 public class User{
+    private String name;
+    private String country;
+    private Cart cart;
+    private WishList wishList;
+
 
 }
