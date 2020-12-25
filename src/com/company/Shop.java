5e3f581bee2bb7c561af1846b6251241512c9569
Ivan Valentinov <ivan156drs@gmail.com>
2020-12-25 21:41:56 +0200

    Updated classes.

--- src/com/company/Shop.java
+++ src/com/company/Shop.java
@@ -1,4 +1,21 @@
 package com.company;
 
+import com.company.entities.Product;
+import com.company.people.Producer;
+import com.company.people.User;
+
+import java.util.ArrayList;
+
 public class Shop {
+    private ArrayList<User> users;
+    private ArrayList<Producer> producers;
+    private ArrayList<Product> products;
+
+    public Shop(){
+        users = new ArrayList<>();
+        producers = new ArrayList<>();
+        products = new ArrayList<>();
+    }
+
+
 }
