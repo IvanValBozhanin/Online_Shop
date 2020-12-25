cd2901b493c1f73daf392682589aeb44564f8956
Ivan Valentinov <ivan156drs@gmail.com>
2020-12-25 21:42:17 +0200

    Updated classes.

--- src/com/company/lists/Cart.java
+++ src/com/company/lists/Cart.java
@@ -1,4 +1,15 @@
 package com.company.lists;
 
-public class Cart {
+import java.util.ArrayList;
+
+public class Cart extends ProductContainer{
+
+    public Cart(){
+        products = new ArrayList<>();
+    }
+
+    @Override
+    public void printList() {
+
+    }
 }
