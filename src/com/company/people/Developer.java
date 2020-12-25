8df968860398810201ded10046b1b3af22535bc9
Ivan Valentinov <ivan156drs@gmail.com>
2020-12-25 21:42:01 +0200

    Updated classes.

--- src/com/company/people/Developer.java
+++ src/com/company/people/Developer.java
@@ -1,5 +1,6 @@
 package com.company.people;
 
+
 import com.company.entities.Computer;
 import com.company.entities.Phone;
 import java.util.ArrayList;
@@ -16,28 +17,62 @@ public class Developer extends Producer{
     }
 
     public void developComputer(String nameOfComputer){
+        try{
+            Computer computer = new Computer(nameOfComputer, this);
+            if(computers.contains(computer)){
+                throw new Exception(String.format("——— Model '%s' already exists! ———", nameOfComputer));
+            }
+            computers.add(computer);
+        } catch (Exception exception){
+            System.out.println(exception.getMessage());
+        }
+    }
 
+    public void developPhone(String nameOfPhone){
+        try {
+            Phone phone = new Phone(nameOfPhone, this);
+            if(phones.contains(phone)){
+                throw new Exception(String.format("——— Model '%s' already exists! ———", nameOfPhone));
+            }
+            phones.add(phone);
+        } catch (Exception exception){
+            System.out.println(exception.getMessage());
+        }
     }
 
     @Override
-    public void getProducts() {
+    public void printProducts() {
         getComputers();
         getPhones();
     }
 
     public void getComputers(){
-        System.out.printf("%s", computers.get(0).getName());
-        for(int i=1; i < computers.size(); ++i){
-            System.out.printf(", %s", computers.get(i).getName());
+        try {
+            if (computers.isEmpty()) {
+                throw new Exception(String.format("——— Developer '%s' has not developed any computer yet! ———", this.getName()));
+            }
+            System.out.printf("%s", computers.get(0).getName());
+            for (int i = 1; i < computers.size(); ++i) {
+                System.out.printf(", %s", computers.get(i).getName());
+            }
+            System.out.println();
+        } catch (Exception exception){
+            System.out.println(exception.getMessage());
         }
-        System.out.println();
     }
 
     public void getPhones(){
-        System.out.printf("%s", phones.get(0).getName());
-        for(int i=1; i < phones.size(); ++i){
-            System.out.printf(", %s", phones.get(i).getName());
+        try {
+            if (phones.isEmpty()) {
+                throw new Exception(String.format("——— Developer '%s' has not developed any phones yet! ———", this.getName()));
+            }
+            System.out.printf("%s", phones.get(0).getName());
+            for (int i = 1; i < phones.size(); ++i) {
+                System.out.printf(", %s", phones.get(i).getName());
+            }
+            System.out.println();
+        } catch (Exception exception){
+            System.out.println(exception.getMessage());
         }
-        System.out.println();
     }
 }
