7a36ce9cff3c0def5ec8c26398fdbcc3eb80551d
Ivan Valentinov <ivan156drs@gmail.com>
2020-12-25 21:41:53 +0200

    Updated classes.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,29 @@
 package com.company;
 
 import com.company.people.Developer;
-import com.company.people.Producer;
 import com.company.people.Publisher;
 
 public class Main {
 
     public static void main(String[] args) {
+        generateBase();
+
+    }
+
+    public static void generateBase(){
         Publisher p1 = new Publisher("Aux", "EU");
         p1.publishBook("1113");
+
         p1.publishBook("In the beginning");
-        p1.getProducts();
-        Producer d1 = new Developer("Sam", "GB");
+        p1.printProducts();
+        Developer d1 = new Developer("Sam", "GB");
+        d1.developPhone("Naz");
+        d1.developPhone("Miz");
+        d1.developPhone("Ace");
+        d1.developComputer("Lenses");
+        d1.developComputer("Paras");
+        d1.developComputer("Lenses");
+        d1.printProducts();
+
     }
 }
